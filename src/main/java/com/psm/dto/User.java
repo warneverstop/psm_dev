08a034d5840ac5a87026a295fd8d4314de4f0778
su <815235534@qq.com>
2019-05-22 11:57:51 +0800

    ssm框架搭建完成

---
+++ src/main/java/com/psm/dto/User.java
@@ -0,0 +1,40 @@
+package com.psm.dto;
+
+import javax.persistence.*;
+
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    private String name;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/psm/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.psm.mapper;
+
+import com.psm.dto.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
